f4c79a0b008f6efbf308eab23fb3e5ef0a4b649d
Krisztian Zsolt Sallai <krisztian-zsolt.sallai@ubs.com>
2015-10-26 08:13:26 +0100

    Finalizing the CalculationFactoryTests implementation

--- test/ch/pongo/calculator/calculation/CalculationFactoryTests.java
+++ test/ch/pongo/calculator/calculation/CalculationFactoryTests.java
@@ -1,46 +1,74 @@
 package ch.pongo.calculator.calculation;
 
-import static org.junit.Assert.*;
-import ch.pongo.calculator.calculation.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class CalculationFactoryTests {
 
-	@Test
-	public void test_calculationFactoryShouldReturnOnlyBaseCalculations() {
-
-		CalculationFactory calculationFactory = new CalculationFactory();
-		
-		PlusOperator plusOperator = new PlusOperator();
-		MinusOperator minusOperator = new MinusOperator();
-		MultiplicationOperator multiplicationOperator = new MultiplicationOperator();
-		DivideOperator divideOperator = new DivideOperator();
-		
-		assertSame("For + sign factory must return a calculation with a PlusOperator",plusOperator.getClass(),calculationFactory.createCalculation("+").getClass());		
-		assertSame("For - sign factory must return a calculation with a MinusOperator",minusOperator.getClass(),calculationFactory.createCalculation("-").getClass());		
-		assertSame("For * sign factory must return a calculation with a MultiplicationOperator",multiplicationOperator.getClass(),calculationFactory.createCalculation("*").getClass());		
-		assertSame("For / sign factory must return a calculation with a DivideOperator",divideOperator.getClass(),calculationFactory.createCalculation("/").getClass());		
-	}
-
-	@Test(expected=IllegalArgumentException.class)
- 	public void testCreateCalculation_ShouldHandleNull() {
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateCalculation_NullArg() {
 		// Fixture / Given
 		CalculationFactory calculationFactory = new CalculationFactory();
-		
+
 		// When and Then
 		calculationFactory.createCalculation(null);
 	}
-	
-	@Test
- 	public void testCreateCalculation_ShouldReturnPlusOperator() {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateCalculation_InvalidArg() {
 		// Fixture / Given
 		CalculationFactory calculationFactory = new CalculationFactory();
-		
+
+		// When and Then
+		calculationFactory.createCalculation("s");
+	}
+
+	@Test
+	public void testCreateCalculation_ShouldReturnPlusOperator() {
+		// Fixture / Given
+		CalculationFactory calculationFactory = new CalculationFactory();
+
 		// When
 		Calculation calculation = calculationFactory.createCalculation("+");
-	
-		//Then
+
+		// Then
 		assertTrue(calculation instanceof PlusOperator);
+	}
+
+	@Test
+	public void testCreateCalculation_ShouldReturMinusOperator() {
+		// Fixture / Given
+		CalculationFactory calculationFactory = new CalculationFactory();
+
+		// When
+		Calculation calculation = calculationFactory.createCalculation("-");
+
+		// Then
+		assertTrue(calculation instanceof MinusOperator);
+	}
+
+	@Test
+	public void testCreateCalculation_ShouldReturnMultiplicationOperator() {
+		// Fixture / Given
+		CalculationFactory calculationFactory = new CalculationFactory();
+
+		// When
+		Calculation calculation = calculationFactory.createCalculation("*");
+
+		// Then
+		assertTrue(calculation instanceof MultiplicationOperator);
+	}
+
+	@Test
+	public void testCreateCalculation_ShouldReturnDivideOperator() {
+		// Fixture / Given
+		CalculationFactory calculationFactory = new CalculationFactory();
+
+		// When
+		Calculation calculation = calculationFactory.createCalculation("/");
+
+		// Then
+		assertTrue(calculation instanceof DivideOperator);
 	}
 }
